19c736e556246ef3c5c8284c17d8e6284f4f9582
repl.it user <replituser@example.com>
2020-11-19 19:58:19 +0000

    Final LifeTest

--- Life.java
+++ Life.java
@@ -1,55 +1,199 @@
 public class Life implements ILife {
+
+  private static String[] nextGen = { "     ",
+                                      "     ",
+                                      "     ",
+                                      "     ",
+                                      "     " };
+
+  private static String[] AliveReminder = {  "     ",
+                                          "     ",
+                                          "     ",
+                                          "     ",
+                                          "     " };
   
   public static void main(String[] args) {
-    Life l = new Life(new String[] {  "     ",
-                                      "     ",
-                                      " *** ",
-                                      "     ",
-                                      "     " });
+    Life l = new Life(nextGen);
     l = (Life) l.nextGeneration();
   }
 
-
   public Life() {
-    nukeAll();
+    //nukeAll();   //sorry 
   }
 
   public Life(String[] setup) {
     this();
+    
     for (int y = 0; y < setup.length; y++)
       for (int x = 0; x < setup[y].length(); x++)
         if (setup[y].charAt(x) != ' ')
           setAlive(x, y);
   }
 
-
   @Override
   public void nukeAll() {
     // TODO Auto-generated method stub
+    nextGen[0] = "     ";
+    nextGen[1] = "     ";
+    nextGen[2] = "     ";
+    nextGen[3] = "     ";
+    nextGen[4] = "     ";
+  }
 
+  public void nukeAllReminder() {
+    // TODO Auto-generated method stub
+    AliveReminder[0] = "     ";
+    AliveReminder[1] = "     ";
+    AliveReminder[2] = "     ";
+    AliveReminder[3] = "     ";
+    AliveReminder[4] = "     ";
   }
 
   @Override
   public void setAlive(int x, int y) {
     // TODO Auto-generated method stub
+    nextGen[y] = nextGen[y].substring(0,x)+'*'+nextGen[y].substring(x + 1);
+  }
+
+  public void setAliveReminder(int x, int y) {
+    // TODO Auto-generated method stub
+    AliveReminder[y] = AliveReminder[y].substring(0,x)+'*'+AliveReminder[y].substring(x + 1);
 
   }
 
   @Override
   public void setDead(int x, int y) {
     // TODO Auto-generated method stub
-
+    AliveReminder[y] = AliveReminder[y].substring(0,x)+' '+AliveReminder[y].substring(x + 1);
   }
 
   @Override
   public boolean isAlive(int x, int y) {
     // TODO Auto-generated method stub
+    if(nextGen[y].charAt(x) == '*'){      
+      //System.out.println("Is alive works");      
+      return true; 
+    }
     return false;
   }
 
   @Override
   public ILife nextGeneration() {
-    // TODO Auto-generated method stub
-    return null;
+    Life tmp = new Life(nextGen);
+
+    for (int y = 0; y < nextGen.length; y++) {
+      for (int x = 0; x < nextGen[y].length(); x++) {
+
+        if((isAlive(x, y) == true && checkAround(x, y) < 4) && (checkAround(x, y) == 2 || checkAround(x, y) == 3)) {
+          setAliveReminder(x, y);
+        }
+        else if(checkAround(x, y) == 3 && checkAround(x, y) < 4) {     
+          setAliveReminder(x, y);
+        }
+        else {
+          setDead(x, y);
+        }
+      }
+    }
+
+    System.arraycopy(AliveReminder,0,nextGen,0,AliveReminder.length);
+    nukeAllReminder();
+
+    return tmp;
+  }
+
+  public int checkAround(int h, int v)
+   { 
+     int counter = 0;
+
+     //oben links
+     if(h==0 && v==0)
+     {
+       if(isAlive(h + 1, v )) counter++;   //mitte rechts
+       if(isAlive(h +1, v +1)) counter++;   //unten recht
+       if(isAlive(h, v +1)) counter++;   //unten mitte
+     }
+
+     //unten rechts
+     if(h==4 && v==4)
+     {
+       if(isAlive(h - 1, v -1)) counter++;   //oben links
+       if(isAlive(h - 1, v)) counter++;   //mitte links
+       if(isAlive(h , v-1)) counter++;   //oben mitte 
+     }
+
+     //oben rechts
+     if(h==4 && v==0)
+     {
+       if(isAlive(h - 1, v)) counter++;   //mitte links
+       if(isAlive(h, v +1)) counter++;   //unten mitte
+       if(isAlive(h - 1, v +1)) counter++;   //unten links
+     }
+
+     //unten links
+     if(h==0 && v==4)
+     {
+       if(isAlive(h , v-1)) counter++;   //oben mitte 
+       if(isAlive(h + 1, v )) counter++;   //mitte rechts
+       if(isAlive(h + 1, v - 1)) counter++;   //oben rechts
+     }
+
+     //linker rand
+     if (h==0 && v>0 && v!=4)
+     {
+       //if(isAlive(h - 1, v -1)) counter++;   //oben links
+       //if(isAlive(h - 1, v)) counter++;   //mitte links
+       if(isAlive(h, v +1)) counter++;   //unten mitte
+       if(isAlive(h +1, v +1)) counter++;   //unten recht
+       if(isAlive(h , v-1)) counter++;   //oben mitte 
+       if(isAlive(h + 1, v )) counter++;   //mitte rechts
+       if(isAlive(h + 1, v - 1)) counter++;   //oben rechts
+     }
+     
+     //oberer rand
+     if (v==0 && h>0 && h!=4)
+     {
+       if(isAlive(h + 1, v )) counter++;   //mitte rechts
+       if(isAlive(h +1, v +1)) counter++;   //unten recht
+       if(isAlive(h, v +1)) counter++;   //unten mitte
+       //if(isAlive(h + 1, v - 1)) counter++;   //oben rechts
+       //if(isAlive(h , v-1)) counter++;   //oben mitte 
+       if(isAlive(h - 1, v +1)) counter++;   //unten links
+       if(isAlive(h - 1, v)) counter++;   //mitte links  
+     }
+
+    //rechter Rand
+    if (h==4 && v>0 && v!=4)
+    {
+      if(isAlive(h - 1, v -1)) counter++;   //oben links
+      if(isAlive(h , v-1)) counter++;   //oben mitte
+      if(isAlive(h, v +1)) counter++;   //unten mitte 
+      if(isAlive(h - 1, v +1)) counter++;   //unten links
+      if(isAlive(h - 1, v)) counter++;   //mitte links
+    }
+
+    //unten Rand
+    if (v==4 && h>0 && h!=4)
+    {
+      if(isAlive(h - 1, v -1)) counter++;   //oben links
+      if(isAlive(h , v-1)) counter++;   //oben mitte
+      if(isAlive(h + 1, v - 1)) counter++;   //oben rechts
+      if(isAlive(h + 1, v )) counter++;   //mitte rechts
+      if(isAlive(h - 1, v)) counter++;   //mitte links
+    }
+
+     if(h>0 && h<4 && v>0 && v<4)
+     {
+       if(isAlive(h - 1, v -1)) counter++;   //oben links
+       if(isAlive(h , v-1)) counter++;   //oben mitte
+       if(isAlive(h + 1, v - 1)) counter++;   //oben rechts
+       if(isAlive(h + 1, v )) counter++;   //mitte rechts
+       if(isAlive(h +1, v +1)) counter++;   //unten rechts
+       if(isAlive(h, v +1)) counter++;   //unten mitte 
+       if(isAlive(h - 1, v +1)) counter++;   //unten links
+       if(isAlive(h - 1, v)) counter++;   //mitte links
+       //System.out.println("Counter: " + counter);
+     }
+     return(counter);
   }
 }
--- LifeTest.java
+++ LifeTest.java
@@ -6,33 +6,76 @@ public class LifeTest {
     @Test
     public void createNewCell() {
         
-        // Arrange: drei lebende Zellen
-        Life l = new Life();
-        l.setAlive(0, 0);
-        l.setAlive(0, 1);
-        l.setAlive(0, 2);
+      // Arrange: drei lebende Zellen
+      Life l = new Life();
+      l.nukeAll();
+      l.setAlive(0, 0);
+      l.setAlive(0, 1);
+      l.setAlive(0, 2);
 
-        // Act: Berechnung der Folgegeneration
-        ILife nextGen = l.nextGeneration();
+      // Act: Berechnung der Folgegeneration
+      ILife nextGener = l.nextGeneration();
 
-        // Assert: Rasterpunkt mit drei Nachbarn sollte jetzt leben
-        assertTrue(nextGen.isAlive(1, 1));
+      // Assert: Rasterpunkt mit drei Nachbarn sollte jetzt leben
+      assertTrue(nextGener.isAlive(1, 1));
     }
+    
 
 
     @Test
     public void destroyLonelyCell() {
+      Life l = new Life();
+      l.nukeAll();
+      l.setAlive(1, 0);
+        
+      ILife nextGen = l.nextGeneration();
+
+      assertFalse(nextGen.isAlive(1, 0));
     }
 
 
     @Test
     public void keepAliveCell() {
-    }
+      Life l = new Life();
+      l.nukeAll();
+      l.setAlive(0, 0);
+      l.setAlive(1, 0);
+      l.setAlive(2, 0);
 
+      ILife nextGen = l.nextGeneration();
+
+      assertTrue(nextGen.isAlive(1, 0));
+    }
 
     @Test
     public void destroyCrowdedCell() {
+      Life l = new Life();
+      l.nukeAll();
+      l.setAlive(0, 0);
+      l.setAlive(0, 1);
+      l.setAlive(0, 2);
+      l.setAlive(1, 0);
+      l.setAlive(1, 1);
+
+      // Act: Berechnung der Folgegeneration
+      ILife nextGen = l.nextGeneration();
+
+      // Assert: Rasterpunkt mit drei Nachbarn sollte jetzt leben
+      assertFalse(nextGen.isAlive(1, 1));
+
+      l.nukeAll();
+      l.setAlive(0, 0);
+      l.setAlive(0, 1);
+      l.setAlive(0, 2);
+      l.setAlive(1, 0);
+      l.setAlive(1, 1);
+      l.setAlive(2, 1);
+      l.setAlive(2, 2);
+
+      // Act: Berechnung der Folgegeneration
+      nextGen = l.nextGeneration();
+
+      // Assert: Rasterpunkt mit drei Nachbarn sollte jetzt leben
+      assertFalse(nextGen.isAlive(1, 1));
     }
-
-
 }
